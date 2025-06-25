1307fa0dc255abd38c95f5e2ce4c1c4ca45e9576
Pratyush Panwar <avipanwar1212@gmail.com>
2025-06-25 11:35:46 +0530

    updat header

---
+++ src/Input.jsx
@@ -0,0 +1,31 @@
+import React, {useId} from 'react'
+
+function Input({
+    label,
+    type="text",
+    className="",
+    ref,
+    ...props
+}) {
+
+    const id = useId()
+    return (
+        <div>
+            {lable && (
+                <label htmlFor={id} className="inline-block mb-1 pl-1">
+                    {label}
+                </label>
+            )}
+
+            <input 
+            id={id}
+            type={type}
+            ref={ref}
+            className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+            {...props}
+            />
+        </div>
+    )
+}
+
+export default Input
--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,15 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import container from "./container/container"
 
 
 
 export {
     Header,
     Footer,
-    
+    Logo,
+    LogoutBtn,
+    container,
 }
